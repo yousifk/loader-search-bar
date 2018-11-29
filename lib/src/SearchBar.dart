13e6546a2a24b19cbcfa219f97f9ccd6d10c42ee
tomwyr <tomwyr@hotmail.com>
2018-11-29 13:24:26 +0100

    Create base state and builder interfaces

--- lib/src/SearchBar.dart
+++ lib/src/SearchBar.dart
@@ -1,15 +1,14 @@
-import 'dart:async';
 import 'dart:ui';
 
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:loader_search_bar/src/AutoActive.dart';
-import 'package:loader_search_bar/src/StateHolder.dart';
+import 'package:loader_search_bar/src/SearchBarController.dart';
+import 'package:loader_search_bar/src/SearchBarState.dart';
 
 import 'QuerySetLoader.dart';
 import 'SearchBarAttrs.dart';
-import 'SearchBarBuilder.dart';
 import 'SearchItem.dart';
 
 /// Search field widget being displayed within Scaffold element.
@@ -116,12 +115,12 @@ class SearchBar extends StatefulWidget implements PreferredSizeWidget {
       ? _getAvailableSpace ?? attrs.searchBarSize
       : attrs.searchBarSize;
 
-  bool get _shouldTakeWholeSpace =>
-      loader != null && (_isThisOrLastActivated ?? false);
+  bool get _shouldTakeWholeSpace => loader != null && _isThisOrLastActivated;
 
   bool get _isThisOrLastActivated =>
-      SearchBarState._stateHolder[this]?.activated ??
-      SearchBarState._stateHolder.lastOrNull?.activated;
+      SearchBarState.stateHolder[this]?.activated ??
+      SearchBarState.stateHolder.lastOrNull?.activated ??
+      false;
 
   Size get _getAvailableSpace {
     final screenSize = MediaQueryData.fromWindow(window).size;
@@ -129,224 +128,5 @@ class SearchBar extends StatefulWidget implements PreferredSizeWidget {
   }
 
   @override
-  State createState() => SearchBarState();
-}
-
-class SearchBarState extends State<SearchBar> {
-  static final _stateHolder = StateHolder<SearchBar, SearchBarState>();
-
-  bool activated = false;
-
-  bool isClearingQuery = false;
-
-  bool expanded;
-
-  bool _hasPendingScaffold = false;
-
-  FocusNode searchFocusNode = FocusNode();
-
-  TextEditingController queryInputController;
-
-  String loaderQuery;
-
-  Orientation currentOrientation;
-
-  bool get focused => searchFocusNode.hasFocus;
-
-  bool get queryNotEmpty => queryInputController.text.isNotEmpty;
-
-  QuerySetLoader get _safeLoader => widget.loader ?? QuerySetLoader.blank;
-
-  EdgeInsets get screenPadding => MediaQuery.of(context).padding;
-
-  VoidCallback get clearQueryCallback =>
-      widget.controller?.onClearQuery ?? _onClearQuery;
-
-  VoidCallback get cancelSearchCallback =>
-      widget.controller?.onCancelSearch ?? _onCancelSearch;
-
-  @override
-  void initState() {
-    super.initState();
-    _stateHolder.add(this);
-    expanded = !widget.iconified;
-    searchFocusNode.addListener(_onSearchFocusChange);
-    _initSearchQuery();
-    _initAutoActive();
-  }
-
-  void _onSearchFocusChange() {
-    if (focused && !activated) {
-      setState(() => _updateActivated(true));
-    }
-  }
-
-  void _initSearchQuery() {
-    queryInputController?.dispose();
-    queryInputController = TextEditingController(text: widget.initialQuery)
-      ..addListener(_onQueryControllerChange);
-  }
-
-  void _onQueryControllerChange() {
-    if (isClearingQuery) {
-      isClearingQuery = false;
-      onTextChange('');
-    }
-  }
-
-  void onTextChange(String text) {
-    if (_safeLoader.loadOnEachChange) {
-      setState(() => loaderQuery = text);
-    }
-    if (widget.onQueryChanged != null) widget.onQueryChanged(text);
-  }
-
-  void onTextSubmit(String text) {
-    if (!_safeLoader.loadOnEachChange) {
-      setState(() => loaderQuery = text);
-    }
-    if (widget.onQuerySubmitted != null) widget.onQuerySubmitted(text);
-  }
-
-  void _initAutoActive() {
-    if (widget.autoActive.shouldActivate(this)) {
-      Future(() => _activate(false));
-    }
-  }
-
-  void onSearchAction() {
-    _initSearchQuery();
-    setState(() {
-      expanded = true;
-      _updateActivated(true);
-    });
-  }
-
-  void _onCancelSearch() {
-    searchFocusNode.unfocus();
-    widget.loader?.clearData();
-    setState(() {
-      if (widget.iconified) expanded = false;
-      loaderQuery = null;
-      _updateActivated(false);
-    });
-  }
-
-  void _updateActivated(bool value) {
-    if (activated != value) {
-      activated = value;
-      widget.controller?.onActivatedChanged?.call(value);
-      if (activated) {
-        loaderQuery = queryInputController.text;
-      }
-      _rebuildScaffold();
-    }
-  }
-
-  void _onClearQuery() =>
-      queryNotEmpty ? _clearQueryField() : searchFocusNode.unfocus();
-
-  void _clearQueryField() {
-    isClearingQuery = true;
-    queryInputController.clear();
-    _requestSearchFocus();
-  }
-
-  void _activate(bool forceFocus) {
-    if (!activated) {
-      if (widget.iconified) {
-        onSearchAction();
-      } else {
-        _activateMerged();
-      }
-    }
-    if (activated && forceFocus && !focused) {
-      _focusSearchField();
-    }
-  }
-
-  void _activateMerged() {
-    if (widget.autofocus) {
-      _requestSearchFocus();
-    } else {
-      setState(() => _updateActivated(true));
-    }
-  }
-
-  void _focusSearchField() async {
-    await Future.doWhile(() => Future(() => _hasPendingScaffold));
-    _requestSearchFocus();
-  }
-
-  void _rebuildScaffold() {
-    if (!_hasPendingScaffold) {
-      _hasPendingScaffold = true;
-      Future.delayed(Duration(milliseconds: 50), () {
-        Scaffold.of(context).setState(() {});
-        _hasPendingScaffold = false;
-      });
-    }
-  }
-
-  void onPrefixSearchTap() {
-    _requestSearchFocus();
-    _highlightQueryText();
-  }
-
-  void _requestSearchFocus() =>
-      FocusScope.of(context).requestFocus(searchFocusNode);
-
-  void _highlightQueryText() {
-    queryInputController.selection = TextSelection(
-      baseOffset: queryInputController.value.text.length,
-      extentOffset: 0,
-    );
-  }
-
-  @override
-  void dispose() {
-    searchFocusNode.dispose();
-    queryInputController.dispose();
-    _stateHolder.remove(this);
-    super.dispose();
-  }
-
-  Future<bool> onWillPop() {
-    bool shouldPop;
-    if (activated) {
-      _onCancelSearch();
-      shouldPop = false;
-    } else {
-      shouldPop = true;
-    }
-    return Future.value(shouldPop);
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    widget.controller?._state = this;
-    return SearchBarBuilder(this, context);
-  }
-}
-
-class SearchBarController {
-  SearchBarController({
-    this.onCancelSearch,
-    this.onClearQuery,
-    this.onActivatedChanged,
-  });
-
-  final VoidCallback onCancelSearch;
-  final VoidCallback onClearQuery;
-  final ValueChanged<bool> onActivatedChanged;
-
-  SearchBarState _state;
-
-  void setQueryText(String text) => _state?.queryInputController?.text = text;
-  void startSearch({forceFocus = true}) => _state?._activate(forceFocus);
-  void cancelSearch() => _state?._onCancelSearch();
-  void clearQuery() => _state?._onClearQuery();
-  bool get isEmpty => _state != null ? !_state.queryNotEmpty : null;
-  bool get isActivated => _state.activated;
-  bool get isFocused => _state.focused;
+  State createState() => SearchBarState.create(this);
 }
