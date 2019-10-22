7c0211faa615bb79ad44533d41782e93dd221210
Yousif Khalid <yousifk55@gmail.com>
2019-10-22 18:37:32 +0300

    update and fix

---
+++ example/ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=C:\SDK\flutter"
+export "FLUTTER_APPLICATION_PATH=C:\Users\yk\Documents\GitHub\loader-search-bar\example"
+export "FLUTTER_TARGET=lib\main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build\ios"
+export "FLUTTER_FRAMEWORK_DIR=C:\SDK\flutter\bin\cache\artifacts\engine\ios"
